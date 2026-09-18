109010addc514eda037aa177c0078a71577f25a3
Guillaume Boudreau <guillaume@pommepause.com>
2026-09-18 19:55:55 -0400

    version bump

--- ws_api/__init__.py
+++ ws_api/__init__.py
@@ -9,7 +9,7 @@ from ws_api.exceptions import (
 from ws_api.session import WSAPISession
 from ws_api.wealthsimple_api import WealthsimpleAPI
 
-__version__ = "0.39.1"
+__version__ = "0.39.3"
 
 __all__ = [
     "CurlException",
